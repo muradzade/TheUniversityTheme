6362c44035689909b469fd807cb0faf0f468c97c
muradzade <58004533+muradzade@users.noreply.github.com>
2021-01-15 17:40:41 +0400

    Add files via upload

---
+++ TheUniversityTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The University Theme",
+    Author = "MRD",
+    Website = "https://github.com/muradzade",
+    Version = "1.0",
+    Description = "Provide to make pages for university"
+)]
